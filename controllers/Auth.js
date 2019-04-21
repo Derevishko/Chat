367e1b5dc825174c9a2a5202b7b04081a3f7c5f6
Andrew <aderevyashko@renault-belarus.by>
2019-04-21 12:16:47 +0300

    add controller for auth

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,1 +1,35 @@
+class Auth{
+  constructor() {
+    this.hash = require('../models/Hash');
+    this.userModel = require('../models/UserModel')
+  };
 
+  get reg() {
+    return async (req, res) => {
+      try {
+        await this.userModel.createUser(req.body)
+        res.status(204);
+      } catch (err) {
+        res.status(err.message);
+      } finally {
+        res.end(); 
+      }
+    }
+  };
+
+  get log() {
+    return async (req, res) => {
+      try {
+        await this.userModel.findUser(req.body);
+        const token = this.hash.createHash( req.body.login + +Date.now() );
+        res.status(200).write(token);
+      } catch (err) {
+        res.status(err.message);
+      } finally {
+        res.end();
+      }
+    }
+  }
+}
+
+module.exports = new Auth();
